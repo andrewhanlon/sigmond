706df4d63a576b9c022ae7b60840e9bb8910d5fd
Andrew D. Hanlon <ddhanlon@gmail.com>
2016-11-07 01:46:18 -0500

    Had a silly mistake in which I was taking the absolute value of every MC estimate.

--- source/tasks/task_average_momentum.cc
+++ source/tasks/task_average_momentum.cc
@@ -89,10 +89,10 @@ void store_in_memory(MCObsHandler *m_obs, CorrelatorAtTimeInfo& corrt_result,
       double im_mean=0.0; double im_err=1.0;
       MCEstimate est;
       est=m_obs->getJackknifeEstimate(obskeyRe);
-      re_mean=std::abs(est.getFullEstimate());
+      re_mean=est.getFullEstimate();
       re_err=est.getSymmetricError();
       est=m_obs->getJackknifeEstimate(obskeyIm);
-      im_mean=std::abs(est.getFullEstimate());
+      im_mean=est.getFullEstimate();
       im_err=est.getSymmetricError();
       xml_result.put_child("Value", "("+to_string(re_mean)+"+/-"+to_string(re_err)+","+to_string(im_mean)+"+/-"+to_string(im_err)+")");
    }
@@ -106,8 +106,18 @@ int relative_sign(double re_mean, double im_mean, double re_comp_mean, double im
  bool imag = (im_mean > 0.) ^ (im_comp_mean < 0.);
 
  if (real != imag) {
+   /*
+   cout << "(" << re_mean << "," << im_mean << ")" << " compared to "
+        << "(" << re_comp_mean << "," << im_comp_mean << ")" << endl;
    cerr << "Relative Sign Issue" << endl;
    exit(1);
+   */
+   if (abs(re_comp_mean) > abs(im_comp_mean)) {
+     imag = real;
+   }
+   else {
+     real = imag;
+   }
  }
 
  if (real) return 1;
@@ -140,10 +150,10 @@ vector<double> get_coefs(MCObsHandler *m_obs, CorrelatorAtTimeInfo& corrt, vecto
  double im_mean=0.0; double im_err=1.0;
  MCEstimate est;
  est=m_obs->getJackknifeEstimate(obskeyRe);
- re_mean=std::abs(est.getFullEstimate());
+ re_mean=est.getFullEstimate();
  re_err=est.getSymmetricError();
  est=m_obs->getJackknifeEstimate(obskeyIm);
- im_mean=std::abs(est.getFullEstimate());
+ im_mean=est.getFullEstimate();
  im_err=est.getSymmetricError();
 
  xml_corr_first.put_child("Value", "("+to_string(re_mean)+"+/-"+to_string(re_err)+","+to_string(im_mean)+"+/-"+to_string(im_err)+")");
@@ -175,10 +185,10 @@ vector<double> get_coefs(MCObsHandler *m_obs, CorrelatorAtTimeInfo& corrt, vecto
        xmlout.put_child(xml_err);
        throw(string("Data not found in same correlator"));}
     est=m_obs->getJackknifeEstimate(obskeyCompRe);
-    re_comp_mean=std::abs(est.getFullEstimate());
+    re_comp_mean=est.getFullEstimate();
     re_comp_err=est.getSymmetricError();
     est=m_obs->getJackknifeEstimate(obskeyCompIm);
-    im_comp_mean=std::abs(est.getFullEstimate());
+    im_comp_mean=est.getFullEstimate();
     im_comp_err=est.getSymmetricError();
 
     xml_corr.put_child("Value", "("+to_string(re_comp_mean)+"+/-"+to_string(re_comp_err)+","+to_string(im_comp_mean)+"+/-"+to_string(im_comp_err)+")");
