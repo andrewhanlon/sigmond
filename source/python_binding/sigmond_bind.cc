c15ab7a254f6539a4e37be20f0ac479bdc72b99e
Andrew D. Hanlon <ahanlon@uni-mainz.de>
2019-03-04 00:53:49 +0100

    Added more functions to sigmond bind

--- source/python_binding/sigmond_bind.cc
+++ source/python_binding/sigmond_bind.cc
@@ -18,9 +18,11 @@
 #include <samplings_handler.h>
 #include <mc_estimate.h>
 #include <task_utils.h>
+#include <momenta.h>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
+#include <pybind11/operators.h>
 
 namespace py = pybind11;
 
@@ -68,20 +70,91 @@ PYBIND11_MODULE(sigmondbind, m) {
     .def(py::init<const std::string &, const std::string &>())
     .def("set_from_string", &XMLHandler::set_from_string);
 
+  py::class_<Momentum>(m, "Momentum")
+    .def(py::init<int, int, int>());
+
   py::enum_<OperatorInfo::OpKind>(m, "OpKind")
     .value("BasicLapH", OperatorInfo::OpKind::BasicLapH)
     .value("GenIrrep", OperatorInfo::OpKind::GenIrrep);
 
   py::class_<OperatorInfo>(m, "OperatorInfo")
     .def(py::init<const std::string &, OperatorInfo::OpKind>())
-    .def(py::init<const GenIrrepOperatorInfo &>());
+    .def(py::init<const BasicLapHOperatorInfo &>())
+    .def(py::init<const GenIrrepOperatorInfo &>())
+    .def("isBasicLapH", &OperatorInfo::isBasicLapH)
+    .def("isGenIrrep", &OperatorInfo::isGenIrrep)
+    .def("getBasicLapH", &OperatorInfo::getBasicLapH)
+    .def("getGenIrrep", &OperatorInfo::getGenIrrep)
+    .def("output", (std::string (OperatorInfo::*)(bool, int) const) &OperatorInfo::output)
+    .def("str", &OperatorInfo::str)
+    .def("short_output", &OperatorInfo::short_output)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def(py::self <  py::self);
+
+  py::class_<BasicLapHOperatorInfo>(m, "BasicLapHOperatorInfo")
+    .def(py::init<const std::string &>())
+    .def("getNumberOfHadrons", &BasicLapHOperatorInfo::getNumberOfHadrons)
+    .def("isGlueball", (bool (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::isGlueball)
+    .def("isMeson", (bool (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::isMeson)
+    .def("isBaryon", (bool (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::isBaryon)
+    .def("isMesonMeson", &BasicLapHOperatorInfo::isMesonMeson)
+    .def("isMesonBaryon", &BasicLapHOperatorInfo::isMesonBaryon)
+    .def("getMomentum", (Momentum (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::getMomentum)
+    .def("getLGIrrep", (std::string (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::getLGIrrep)
+    .def("getLGClebschGordonIdNum", &BasicLapHOperatorInfo::getLGClebschGordonIdNum)
+    .def("getLGIrrepRow", &BasicLapHOperatorInfo::getLGIrrepRow)
+    .def("getIsospin", &BasicLapHOperatorInfo::getIsospin)
+    .def("getIsospinClebschGordonIdNum", &BasicLapHOperatorInfo::getIsospinClebschGordonIdNum)
+    .def("getFlavor", (std::string (BasicLapHOperatorInfo::*)() const) &BasicLapHOperatorInfo::getFlavor)
+    .def("getFlavorCode", &BasicLapHOperatorInfo::getFlavorCode)
+    .def("getHadronFlavor", (std::string (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::getFlavor)
+    .def("getHadronStrangeness", (int (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::getStrangeness)
+    .def("isHadronGlueball", (bool (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::isGlueball)
+    .def("isHadronMeson", (bool (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::isMeson)
+    .def("isHadronBaryon", (bool (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::isBaryon)
+    .def("isHadronFermion", &BasicLapHOperatorInfo::isFermion)
+    .def("isHadronBoson", &BasicLapHOperatorInfo::isBoson)
+    .def("getHadronLGIrrep", (std::string (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::getLGIrrep)
+    .def("getHadronSpatialType", &BasicLapHOperatorInfo::getSpatialType)
+    .def("getHadronSpatialIdNumber", &BasicLapHOperatorInfo::getSpatialIdNumber)
+    .def("getHadronDisplacementLength", &BasicLapHOperatorInfo::getDisplacementLength)
+    .def("getHadronMomentum", (Momentum (BasicLapHOperatorInfo::*)(uint) const) &BasicLapHOperatorInfo::getMomentum)
+    .def("output", (std::string (BasicLapHOperatorInfo::*)(bool, int) const) &BasicLapHOperatorInfo::output)
+    .def("str", &BasicLapHOperatorInfo::str)
+    .def("short_output", &BasicLapHOperatorInfo::short_output)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def(py::self <  py::self);
 
   py::class_<GenIrrepOperatorInfo>(m, "GenIrrepOperatorInfo")
-    .def(py::init<const std::string &>());
+    .def(py::init<const std::string &>())
+    .def("getMomentum", &GenIrrepOperatorInfo::getMomentum)
+    .def("getMomentumSquared", &GenIrrepOperatorInfo::getMomentumSquared)
+    .def("hasDefiniteMomentum", &GenIrrepOperatorInfo::hasDefiniteMomentum)
+    .def("getLGIrrep", &GenIrrepOperatorInfo::getLGIrrep)
+    .def("getLGIrrepRow", &GenIrrepOperatorInfo::getLGIrrepRow)
+    .def("getIsospin", &GenIrrepOperatorInfo::getIsospin)
+    .def("getStrangeness", &GenIrrepOperatorInfo::getStrangeness)
+    .def("getIDName", &GenIrrepOperatorInfo::getIDName)
+    .def("getIDIndex", &GenIrrepOperatorInfo::getIDIndex)
+    .def("output", (std::string (GenIrrepOperatorInfo::*)(bool, int) const) &GenIrrepOperatorInfo::output)
+    .def("str", &GenIrrepOperatorInfo::str)
+    .def("short_output", &GenIrrepOperatorInfo::short_output)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def(py::self <  py::self);
 
   py::class_<CorrelatorInfo>(m, "CorrelatorInfo")
     .def(py::init<const OperatorInfo &, const OperatorInfo &>())
-    .def("str", &CorrelatorInfo::str);
+    .def("getSource", &CorrelatorInfo::getSource)
+    .def("getSink", &CorrelatorInfo::getSink)
+    .def("isSinkSourceSame", &CorrelatorInfo::isSinkSourceSame)
+    .def("output", (std::string (CorrelatorInfo::*)(bool, int) const) &CorrelatorInfo::output)
+    .def("str", &CorrelatorInfo::str)
+    .def(py::self == py::self)
+    .def(py::self != py::self)
+    .def(py::self <  py::self);
 
   py::class_<CorrelatorAtTimeInfo>(m, "CorrelatorAtTimeInfo")
     .def(py::init<const OperatorInfo &, const OperatorInfo &, int, bool, bool, bool>())
