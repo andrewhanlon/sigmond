9ace98ecbe015bbbca414a1b710f2a55d3bfdd9e
xinucode <skinnersarah93@gmail.com>
2023-12-11 18:52:03 -0500

    bug fixes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,8 +2,8 @@
 INSTALL_DIR=/usr/local
 
 git clone https://github.com/HDFGroup/hdf5.git
-git checkout hdf5_1_10_11
 cd hdf5
+git checkout hdf5_1_10_11
 ./configure --prefix=$INSTALL_DIR
 make -j 2
 make check
@@ -11,11 +11,11 @@ sudo make install
 sudo make check-install
 cd ..
 
-wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.11.tar.gz
-tar -xzvf v3.11.tar.gz
-cd lapack-3.11
+wget https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.10.1.tar.gz
+tar -xzvf v3.10.1.tar.gz
+cd lapack-3.10.1
 mkdir build
 cd build
-sudo cmake -DCMAKE_INSTALL_LIBDIR=($INSTALL_DIR)/lib/ ..
+sudo cmake -DCMAKE_INSTALL_LIBDIR=$INSTALL_DIR/lib/ ..
 sudo cmake --build . -j --target install
 cd ../..
